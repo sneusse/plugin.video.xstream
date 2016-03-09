7e8e3e448dfe07a24411b8592b0e6b5359b6aa4b
StoneOffStones <irubin@me.com>
2016-03-09 16:54:04 +0100

    fix name/addon-id

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 from xbmc import translatePath
 
-addonID = 'plugin.video.xstream'
+addonID = 'plugin.video.xstream-beta'
 addon = xbmcaddon.Addon(id = addonID)
 addonPath = translatePath(addon.getAddonInfo('path')).decode('utf-8')
 profilePath = translatePath(addon.getAddonInfo('profile')).decode('utf-8')
