a1ee4bb558c0d5a1f29c984e86906df3e04bc817
StoneOffStones <irubin@me.com>
2016-03-13 18:56:08 +0100

    fix filme-stream_com.py

--- sites/filme-streamz_com.py
+++ sites/filme-streamz_com.py
@@ -12,39 +12,43 @@ SITE_ICON = 'filmestreamz.png'
 URL_MAIN = 'http://filme-streamz.com'
 
 def load():
-    oGui = cGui()
-      
-    oGui.addFolder(cGuiElement('Neuste Filme', SITE_IDENTIFIER, 'ShowNewMovies'))
-    oGui.setEndOfDirectory()
-  
+   oGui = cGui()
+     
+   oGui.addFolder(cGuiElement('Neuste Filme', SITE_IDENTIFIER, 'showNewMovies'))
+   oGui.setEndOfDirectory()
+ 
 def showNewMovies():
-    oGui = cGui()
-    params = ParameterHandler()
-    oRequestHandler = cRequestHandler(URL_MAIN)
-    sHtmlContent = oRequestHandler.request()
-    # parse movie entries
-    pattern = 'class="imborder"<a href="([^"]+)""title="([^"]+)>.*?<img src="([^"]+)>'
-    oParser = cParser()
-    aResult = oParser.parse
-    if not aResult[0]:
-       total = len(aResult[1])
-    for link, title, img in aResult[1]:
-        titleParse = tile.split('(')
-        movieTitle = TitleParse[0].strip().decode('iso-8859-1').encode('utf-8')
-        guiElement = cGuiElement(movieTitle, SITE_IDENTIFIER, 'getHoster')
-        guiElement.setThumbnail(img)
-        guiElement.setDescription(plot.decode('iso-8859-1'))
-        if len(titleParts)>1:
-            tag = titleParts[-1].replace(')','')
-            if tag.isdigit() and len(tag)==4:
-                guiElement.setYear(tag)
-        guiElement.setMediaType('movie')
-        if '720' in qual:
-            guiElement._sQuality = 720
-        elif '1080p' in qual:
-            guiElement._sQuality = 1080
+   oGui = cGui()
+   params = ParameterHandler()
+   oRequestHandler = cRequestHandler(URL_MAIN)
+   sHtmlContent = oRequestHandler.request()
+   # parse movie entries
+   pattern = '<li[^>]*class="imgborder".*?<a[^>]*href="([^"]*)"[^>]*title="([^"]*)"[^>]*>.*?</a[>].*?<img[^>]*src="([^"]*)"[^>]*class="scale-with-grid"[^>]*>.*?'
+   oParser = cParser()
+   aResult = oParser.parse(sHtmlContent, pattern)
 
-        params.setParam('siteUrl',link)
-        oGui.addFolder(guiElement, params, bIsFolder = False, iTotal =total)
-    oGui.setView('movies')
-    oGui.setEndOfDirectory()
+   if not aResult[0]:
+      return
+
+   total = len(aResult[1])
+
+   for link, title, img in aResult[1]:
+       titleParse = title.split('(')
+       movieTitle = titleParse[0].strip().decode('iso-8859-1').encode('utf-8')
+       guiElement = cGuiElement(movieTitle, SITE_IDENTIFIER, 'getHoster')
+       guiElement.setThumbnail(img)
+       #guiElement.setDescription(plot.decode('iso-8859-1'))
+       if len(titleParse)>1:
+           tag = titleParse[-1].replace(')','')
+           if tag.isdigit() and len(tag)==4:
+               guiElement.setYear(tag)
+       guiElement.setMediaType('movie')
+       #if '720' in qual:
+       #    guiElement._sQuality = 720
+       #elif '1080p' in qual:
+       #    guiElement._sQuality = 1080
+
+       params.setParam('siteUrl',link)
+       oGui.addFolder(guiElement, params, bIsFolder = False, iTotal =total)
+   oGui.setView('movies')
+   oGui.setEndOfDirectory()
