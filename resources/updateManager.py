6f392dea84bc07b5f6ee8810c0dfdbb6eb091103
sraedler <simon.raedler@yahoo.de>
2016-03-14 23:49:09 +0100

    added setting to choose update branch added logging added progressbar

--- resources/updateManager.py
+++ resources/updateManager.py
@@ -4,6 +4,8 @@ import xml.etree.ElementTree as ET
 import zipfile
 import logger
 import xbmc
+import xbmcgui
+from resources.lib.config import cConfig
 from distutils.version import LooseVersion as V
 from resources.lib import common
 
@@ -13,10 +15,12 @@ TEMP_DIR = os.path.join(ROOT_DIR, "TEMP")
 XSTREAM_DIRNAME = os.path.basename(ROOT_DIR)
 
 ## Remote path to download plugin.zip and version file.
-REMOTE_PATH = "https://github.com/StoneOffStones/plugin.video.xstream/archive/beta.zip"
+REMOTE_PATH_BETA = "https://github.com/StoneOffStones/plugin.video.xstream/archive/beta.zip"
+REMOTE_PATH_MASTER = "https://github.com/Lynx187/plugin.video.xstream/archive/master.zip"
 
 ## Full path of the remote file version.
-REMOTE_VERSION_FILE = "https://raw.githubusercontent.com/StoneOffStones/plugin.video.xstream/beta/addon.xml"
+REMOTE_VERSION_FILE_BETA = "https://raw.githubusercontent.com/StoneOffStones/plugin.video.xstream/beta/addon.xml"
+REMOTE_VERSION_FILE_MASTER = "https://raw.githubusercontent.com/Lynx187/plugin.video.xstream/master/addon.xml"
 
 ## Full path to the local .zip file. It includes the file name.
 LOCAL_FILE = os.path.join(TEMP_DIR, "xStream_update.zip")
@@ -25,11 +29,23 @@ LOCAL_FILE = os.path.join(TEMP_DIR, "xStream_update.zip")
 def checkforupdates():
     logger.info("xStream checkforupdates")
 
+    REMOTE_PATH = REMOTE_PATH_BETA if (cConfig().getSetting('useBeta') == "true") else REMOTE_PATH_MASTER
+    REMOTE_VERSION_FILE = REMOTE_VERSION_FILE_BETA if (cConfig().getSetting('useBeta') == "true") else REMOTE_VERSION_FILE_MASTER
+
+    logger.info("Remote Path: " + REMOTE_PATH)
+
     remoteVersionXML = urllib.urlopen(REMOTE_VERSION_FILE).read()
     remoteVersion = ET.fromstring(remoteVersionXML).attrib['version']
 
+    logger.info("Old Version: " + common.addon.getAddonInfo('version'))
+    logger.info("New Version: " + remoteVersion)
+
     if (V(remoteVersion)>V(common.addon.getAddonInfo('version'))):
         logger.info("New Version Available")
+
+        progressDialog = "Update xStream auf Version: " + ("Beta" if (cConfig().getSetting('useBeta') == "true") else "Master")
+        progress = xbmcgui.DialogProgressBG()
+        progress.create(progressDialog)
 
         if not os.path.exists(LOCAL_FILE):
             os.mkdir(TEMP_DIR)
@@ -38,7 +54,8 @@ def checkforupdates():
 
         updateFile = zipfile.ZipFile(LOCAL_FILE)
 
-        for n in updateFile.namelist():
+        for index, n in enumerate(updateFile.namelist()):
+            percentage = index * 100 / len(updateFile.namelist())
             if n[-1] != "/":
                 dest = os.path.join(ROOT_DIR, "/".join(n.split("/")[1:]))
                 destdir = os.path.dirname(dest)
@@ -50,5 +67,7 @@ def checkforupdates():
                 f = open(dest, 'w')
                 f.write(data)
                 f.close()
+            progress.update(percentage, progressDialog)
         updateFile.close()
         logger.info("Update Successful")
+        progress.close()
