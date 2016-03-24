ecd00381ca8fa42b6a554cd89a61bd0b56f80615
StoneOffStones <irubin@me.com>
2016-03-24 20:06:50 +0100

    clean the branch

--- resources/lib/common.py
+++
@@ -1,7 +0,0 @@
-import xbmcaddon
-from xbmc import translatePath
-
-addonID = 'plugin.video.xstream'
-addon = xbmcaddon.Addon(id = addonID)
-addonPath = translatePath(addon.getAddonInfo('path')).decode('utf-8')
-profilePath = translatePath(addon.getAddonInfo('profile')).decode('utf-8')
