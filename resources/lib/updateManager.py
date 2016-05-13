2175a59280643769bc03fbe9a0c38da80f874ca8
StoneOffStones <irubin@me.com>
2016-05-13 11:53:11 +0200

    Update Remote-URLs for AutoUpdate

--- resources/lib/updateManager.py
+++ resources/lib/updateManager.py
@@ -17,10 +17,10 @@ TEMP_DIR = os.path.join(ROOT_DIR, "TEMP")
 XSTREAM_DIRNAME = os.path.basename(ROOT_DIR)
 
 ## Remote path to download plugin.zip and version file.
-REMOTE_URL_MASTER = "https://github.com/Lynx187/plugin.video.xstream/archive/master.zip"
-REMOTE_URL_BETA = "https://api.github.com/repos/StoneOffStones/plugin.video.xstream/git/refs/tags/"
+REMOTE_URL_MASTER = "https://api.github.com/repos/Lynx187/plugin.video.xstream/git/refs/tags/"
+REMOTE_URL_BETA = "https://github.com/Lynx187/plugin.video.xstream/archive/master.zip"
 REMOTE_URL_NIGHTLY = "https://github.com/StoneOffStones/plugin.video.xstream/archive/nightly.zip"
-
+https://api.github.com/repos/Lynx187/plugin.video.xstream/git/refs/tags/
 ## Full path of the remote file version.
 REMOTE_VERSION_FILE_MASTER = "https://raw.githubusercontent.com/Lynx187/plugin.video.xstream/master/addon.xml"
 
