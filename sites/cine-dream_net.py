ac2e3737ca1c94b0600dd63203be8e1b26815444
Viper2k4 <Viper2k4@users.noreply.github.com>
2016-06-19 23:55:15 +0200

    fixes for cine-dream
    - remove unused parm in load()
    - rename showKategorie to showCategory
    - filter main content of the page (to prevent double entries from "Aktuelle Kinofilme")
    - set oGui based on parameter (for global search)
    - message for user if there is no result (search inside the plugin)
    - fix HTML-Unescape for the title
    - set MediaTypes correct, remove unused parameters on movie entries and set the total len of the list
    - remove some tabs and old comments from other plugins

--- sites/cine-dream_net.py
+++ sites/cine-dream_net.py
@@ -10,10 +10,10 @@ from resources.lib.util import cUtil
 import re
 
 SITE_IDENTIFIER = 'cine-dream_net'
-SITE_NAME = 'CineDream'
+SITE_NAME = 'Cine-Dream'
 SITE_ICON = 'cine-dream_net.png'
 
-URL_MAIN = 'Http://www.cine-dream.net/'
+URL_MAIN = 'http://www.cine-dream.net/'
 URL_SEARCH =  URL_MAIN + '?s=%s'
 
 def load():
@@ -22,95 +22,90 @@ def load():
     params = ParameterHandler()
     params.setParam('sUrl', URL_MAIN)
     oGui.addFolder(cGuiElement('Alle Filme', SITE_IDENTIFIER, 'showEntries'), params)
-    params.setParam('sUrl', URL_MAIN)
-    oGui.addFolder(cGuiElement('Kategorien', SITE_IDENTIFIER, 'showKategorie'), params)
-    params.setParam('sUrl', URL_SEARCH)
+    oGui.addFolder(cGuiElement('Kategorien', SITE_IDENTIFIER, 'showCategory'))
     oGui.addFolder(cGuiElement('Suche', SITE_IDENTIFIER, 'showSearch'))
     oGui.setEndOfDirectory()
-	
-def showKategorie():
+
+def showCategory():
     params = ParameterHandler()
     oGui = cGui()
     sHtmlContent = cRequestHandler(URL_MAIN).request()
     pattern = 'class="cat-item.*?a[^>]*href="([^"]+)" title="([^"]+)' # url / title
     aResult = cParser().parse(sHtmlContent, pattern)
-
     for sUrl, sTitle in aResult[1]:
         params.setParam('sUrl', sUrl)
         oGui.addFolder(cGuiElement(sTitle.strip(), SITE_IDENTIFIER, 'showEntries'), params)
     oGui.setEndOfDirectory()
-	
+
 def showEntries(entryUrl = False, sGui = False):
-    oGui = cGui()
+    oGui = sGui if sGui else cGui()
     params = ParameterHandler()
     if not entryUrl: entryUrl = params.getValue('sUrl')
+
     sHtmlContent = cRequestHandler(entryUrl).request()
-    pattern ='<div class="thumbnail">.*? <a href="([^"]+)" title="([^"]+)">.*?<img src="([^"]+)'
+    aResult = cParser().parse(sHtmlContent, '<h2[^>]*class="maintitle">(.*?)<center') # filter main content if needed
+    if aResult[0]: 
+        sHtmlContent = aResult[1][0]
+
+    pattern = '<div[^>]*class="thumbnail">.*?' # container
+    pattern += '<a[^>]*href="([^"]*)"[^>]*title="([^"]*)"[^>]*>.*?' # linke / title
+    pattern += '<img[^>]*src="([^"]*)"' # image
     aResult = cParser().parse(sHtmlContent, pattern)
 
-    if len(aResult) > 0:
+    if not aResult[0]: 
+        if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
+        return
 
-        for result in aResult[1]:
-            oGuiElement = cGuiElement(result[1].replace('&#8220;', '"').replace('&#8222;', '"').replace('&#8211;', '-').replace('&#038;', '&').replace('&#8217;', '\''), SITE_IDENTIFIER, 'showHosters')
-            oGuiElement.setSiteName(SITE_IDENTIFIER)
-            oGuiElement.setThumbnail(result[2])
-            params.setParam('entryUrl', result[0])
-            params.setParam('sName', result[1])
-            params.setParam('sThumbnail', result[2])
-            oGui.addFolder(oGuiElement, params, bIsFolder = False,)
-            oGui.setView('movies')
-            
-    #check next page
-	pattern = '<a class="nextpostslink".*? href="([^"]+)'
+    total = len (aResult[1])
+    for sEntryUrl, sName, sThumbnail in aResult[1]:
+        oGuiElement = cGuiElement(cUtil().unescape(sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
+        oGuiElement.setThumbnail(sThumbnail)
+        oGuiElement.setMediaType('movie')
+        params.setParam('entryUrl', sEntryUrl)
+        oGui.addFolder(oGuiElement, params, False, total)
+
+    pattern = '<a[^>]*class="nextpostslink"[^>]*href="([^"]+)"'
     aResult = cParser().parse(sHtmlContent, pattern)
     if aResult[0] and aResult[1][0]:
         params.setParam('sUrl', aResult[1][0])
         oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
+
     if not sGui:
+        oGui.setView('movies')
         oGui.setEndOfDirectory()
         return
 
 def showHosters():
-    oParams = ParameterHandler() #Parameter laden
-    sUrl = oParams.getValue('entryUrl')  # Weitergegebenen Urlteil aus den Parametern holen 
-    oRequestHandler = cRequestHandler(sUrl) # gesamte Url zusammesetzen
-    sHtmlContent = oRequestHandler.request()         # Seite abrufen
-    sPattern = '>Stream:.*?([^" ]+).*?<center><a href="([^"]+)'
-    oParser = cParser()
-    aResult = oParser.parse(sHtmlContent, sPattern,)
-    hosters = []                                     # hosterliste initialisieren
-    sFunction='getHosterUrl'                         # folgeFunktion festlegen 
+    oParams = ParameterHandler()
+    sUrl = oParams.getValue('entryUrl')
+    sHtmlContent = cRequestHandler(sUrl).request()
+    sPattern = '>Stream:.*?([^" ]+).*?<center><a href="([^"]+)' # hostername / url
+    aResult = cParser().parse(sHtmlContent, sPattern)
+    hosters = []
     if aResult[1]:
-        for name, aEntry in aResult[1]:
+        for sName, sUrl in aResult[1]:
             hoster = {} 
-            hoster['link'] = aEntry
-            # extract domain name
-            hoster['name'] = name
+            hoster['link'] = sUrl
+            hoster['name'] = sName
             hosters.append(hoster)
-        hosters.append(sFunction)
+    if hosters:
+        hosters.append('getHosterUrl')
     return hosters
 
 def getHosterUrl(sUrl = False):
-    #ParameterHandler erzeugen
-    oParams = ParameterHandler()
-    # URL ermitteln falls nicht �bergeben
-    if not sUrl: sUrl = oParams.getValue('url')
-    logger.info("url %s" % sUrl)
-    # Array mit einem Eintrag f�r Hosterliste erzeugen (sprich direkt abspielen)
+    if not sUrl: sUrl = ParameterHandler().getValue('url')
     results = []
     result = {}
     result['streamUrl'] = sUrl
     result['resolved'] = False
     results.append(result)
     return results
-    # Ergebniss zur�ckliefern
-	
+
 def showSearch():
     oGui = cGui()
     sSearchText = oGui.showKeyBoard()
     if not sSearchText: return
     _search(False, sSearchText)
-    oGui.setView('movies')
     oGui.setEndOfDirectory()
 
 def _search(oGui, sSearchText):
