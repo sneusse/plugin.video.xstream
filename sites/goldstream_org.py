4232a477118c2ed378ce17a4c9ad16c3497495ef
StoneOffStones <irubin@me.com>
2016-06-24 07:37:09 +0200

    complete goldstream_org

--- sites/goldstream_org.py
+++ sites/goldstream_org.py
@@ -5,10 +5,8 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib import logger
 from resources.lib.handler.ParameterHandler import ParameterHandler
-from resources.lib.handler.pluginHandler import cPluginHandler
 from resources.lib.util import cUtil
 from resources.lib.config import cConfig
-import re
 
 SITE_IDENTIFIER = 'goldstream_org'
 SITE_NAME = 'Goldstream'
@@ -17,8 +15,9 @@ SITE_ICON = 'goldstream_org.png'
 URL_MAIN = 'http://goldstream.org/'
 URL_Kinofilme = URL_MAIN + 'Stream/kinofilme/'
 URL_Filme = URL_MAIN + 'Stream/filme/'
-URL_XXX = URL_MAIN + '/Stream/xxx/'
+URL_ADULT = URL_MAIN + '/Stream/xxx/'
 URL_SEARCH =  URL_MAIN + '?s=%s'
+URL_SERIES = URL_MAIN + 'serien/'
 
 URL_GENRES_LIST = {'Abenteuer' : 'Stream/filme/abenteuer', 'Action' : 'Stream/filme/action', 'Animation' : 'Stream/filme/animation', 'Dokumentation' : 'Stream/filme/dokumentation',
                  'Drama' : 'Stream/filme/drama',  'Family' : 'Stream/filme/family',  'Historie' : 'Stream/filme/historie',  'Horror' : 'Stream/filme/horror',
@@ -34,15 +33,16 @@ def load():
     params.setParam('sUrl', URL_Filme)
     oGui.addFolder(cGuiElement('Alle Filme', SITE_IDENTIFIER, 'showEntries'), params)
     oGui.addFolder(cGuiElement('Genre', SITE_IDENTIFIER, 'showGenresList'), params)
+    oGui.addFolder(cGuiElement('Serien', SITE_IDENTIFIER, 'showSeries'))
     if showAdult():
-        params.setParam('sUrl', URL_XXX)
+        params.setParam('sUrl', URL_ADULT)
         oGui.addFolder(cGuiElement('XXX', SITE_IDENTIFIER, 'showEntries'), params)
     oGui.addFolder(cGuiElement('Suche', SITE_IDENTIFIER, 'showSearch'))
     oGui.setEndOfDirectory()
 
 def showAdult():
     oConfig = cConfig()
-    if oConfig.getSetting('showAdult')=='true':    
+    if oConfig.getSetting('showAdult')=='true':
         return True
     return False
 
@@ -54,6 +54,73 @@ def showGenresList():
         oGui.addFolder(cGuiElement(key, SITE_IDENTIFIER, 'showEntries'), params)
     oGui.setEndOfDirectory()
 
+def showSeries():# Danke Lynx187
+    oGui = cGui()
+    params = ParameterHandler()
+    sHtmlContent = cRequestHandler(URL_SERIES).request()
+    pattern = '<h3>([^<]+)</h3>\s*<p>\|.+?</p>'
+    aResult = cParser().parse(sHtmlContent, pattern)
+    if not aResult[0]:
+        oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
+        return
+    total = len(aResult[1])
+    util = cUtil()
+    for name in aResult[1]:
+        oGuiElement = cGuiElement(util.unescape(name.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showSeasons')
+        oGuiElement.setMediaType('tvshow')
+        params.setParam('selectedShow', name)
+        oGui.addFolder(oGuiElement, params, True, total)
+    oGui.setView('tvshows')
+    oGui.setEndOfDirectory()
+
+def showSeasons():# Danke Lynx187
+    oGui = cGui()
+    params = ParameterHandler()
+    selectedShow = params.getValue('selectedShow')
+    sHtmlContent = cRequestHandler(URL_SERIES).request()
+    pattern = '<h3>%s</h3>\s*<p>\|(.*?)</p>' % selectedShow
+    aResult = cParser().parse(sHtmlContent, pattern)
+    pattern =  'href="([^"]+)">([^<]+)<'
+    aResult = cParser().parse(aResult[1][0], pattern)
+    if not aResult[0]:
+        oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
+        return
+    util = cUtil()
+    total = len(aResult[1])
+    for link, name in aResult[1]:
+        oGuiElement = cGuiElement(util.unescape(name.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showEpisodes')
+        oGuiElement.setMediaType('season')
+        params.setParam('sUrl', link)
+        oGui.addFolder(oGuiElement, params, True, total)
+    oGui.setView('seasons')
+    oGui.setEndOfDirectory()
+
+def showEpisodes(entryUrl = False, sGui = False):
+    oGui = sGui if sGui else cGui()
+    params = ParameterHandler()
+    if not entryUrl: entryUrl = params.getValue('sUrl')
+    sHtmlContent = cRequestHandler(entryUrl).request()
+    pattern = '<h2[^>]*class="entry-title"[^>]*><a[^>]*href="([^"]+)[^>]*rel="bookmark".*?Episode (\d+[^"]+).*?' # link / title
+    pattern += '<p><p>(.*?)</p>' # Description
+    aResult = cParser().parse(sHtmlContent, pattern)
+
+    if not aResult[0]:
+        if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
+        return
+
+    total = len(aResult[1])
+    for sEntryUrl, sName, sDescription in sorted(aResult[1]) :
+        oGuiElement = cGuiElement(cUtil().unescape("Folge - " + sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
+        oGuiElement.setDescription(cUtil().unescape(sDescription.decode('utf-8')).encode('utf-8'))
+        oGuiElement.setMediaType('episode')
+        params.setParam('entryUrl', sEntryUrl)
+        oGui.addFolder(oGuiElement, params, False, total)
+
+    if not sGui:
+        oGui.setView('episode')
+        oGui.setEndOfDirectory()
+        return
+		
 def showEntries(entryUrl = False, sGui = False):
     oGui = sGui if sGui else cGui()
     params = ParameterHandler()
@@ -67,16 +134,15 @@ def showEntries(entryUrl = False, sGui = False):
         if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
         return
 
-    total = len (aResult[1])
+    total = len(aResult[1])
     for sEntryUrl, sName, sDescription in aResult[1]:
         oGuiElement = cGuiElement(cUtil().unescape(sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
         oGuiElement.setDescription(cUtil().unescape(sDescription.decode('utf-8')).encode('utf-8'))
         oGuiElement.setMediaType('movie')
         params.setParam('entryUrl', sEntryUrl)
         oGui.addFolder(oGuiElement, params, False, total)
-
-    pattern = '<div[^>]*class="right"><a[^>]*href="([^"]+)"[^>]*>'
-    aResult = cParser().parse(sHtmlContent, pattern)
+        pattern = '<div[^>]*class="right"><a[^>]*href="([^"]+)"[^>]*>'
+        aResult = cParser().parse(sHtmlContent, pattern)
     if aResult[0] and aResult[1][0]:
         params.setParam('sUrl', aResult[1][0])
         oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
