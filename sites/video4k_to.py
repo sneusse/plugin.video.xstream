16331c01144be432e91c0e587d027d71bb17f7ef
sraedler <simon.raedler@yahoo.de>
2016-09-21 16:28:46 +0200

    [video4k] added series and informations

--- sites/video4k_to.py
+++ sites/video4k_to.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import json
+import threading
 from resources.lib.gui.gui import cGui
 from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.requestHandler import cRequestHandler
@@ -41,7 +45,12 @@ def showEntries(sGui = False, sSearchText = None):
     oRequest = cRequestHandler(URL_REQUEST)
     for key in DEFAULT_REQUEST_PARAMS:
         oRequest.addParameters(key, DEFAULT_REQUEST_PARAMS[key])
+
     oRequest.addParameters('type', oParams.getValue('type'))
+
+    if oParams.getValue('iDisplayStart'):
+        oRequest.addParameters('iDisplayStart', oParams.getValue('iDisplayStart'))
+
     if sSearchText:
         oRequest.addParameters('sSearch', sSearchText)
         oRequest.addParameters('type', '')
@@ -54,13 +63,91 @@ def showEntries(sGui = False, sSearchText = None):
         if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
         return
 
-    for id, sName in aResult:
-        oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showHosters')
-        oParams.setParam('id', id)
-        oGui.addFolder(oGuiElement, oParams, False, isHoster=True)
+    threads = []
+    for mId, sName in aResult:
+        t = threading.Thread(target=_addEntry, args=(oGui, sName, mId), name=sName)
+        threads += [t]
+        t.start()
+    for count, t in enumerate(threads):
+        t.join()
+
+    maxEntries = json.loads(sHtmlContent).items()[3][1]
+
+    if maxEntries > int(oParams.getValue('iDisplayStart')) and maxEntries > 50:
+        oParams.setParam('iDisplayStart', int(oParams.getValue('iDisplayStart')) + 50)
+        oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', oParams)
 
     oGui.setEndOfDirectory()
 
+def _addEntry(oGui, sName, mId):
+    oParams = ParameterHandler()
+
+    oParams.setParam('id', mId)
+    info = loadInformation(mId)
+
+    oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showSeasons' if info[4] else 'showHosters')
+
+    isFolder = False
+
+    if info:
+        oGuiElement.setDescription(info[0])
+        oGuiElement.setLanguage(info[3])
+        oGuiElement.setThumbnail('http:' + info[1])
+        oGuiElement.setYear(info[2])
+        oParams.setParam('lang', info[3])
+        isFolder = info[4] is not False
+
+    oGui.addFolder(oGuiElement, oParams, isFolder, isHoster=not isFolder)
+
+def showSeasons(sGui = None):
+    oGui = sGui if sGui else cGui()
+    oParams = ParameterHandler()
+
+    info = loadInformation(oParams.getValue('id'))
+
+    for sSeason in info[4]:
+        oGuiElement = cGuiElement('Staffel ' + sSeason, SITE_IDENTIFIER, 'showEpisode')
+        oParams.setParam('season', sSeason)
+        oGui.addFolder(oGuiElement, oParams)
+
+    oGui.setEndOfDirectory()
+
+def showEpisode(sGui = None):
+    oGui = sGui if sGui else cGui()
+    oParams = ParameterHandler()
+
+    info = loadInformation(oParams.getValue('id'))
+
+    for sEpisode in info[4][oParams.getValue('season')]:
+        oGuiElement = cGuiElement('Episode ' + str(sEpisode), SITE_IDENTIFIER, 'showHosters')
+        oParams.setParam('episode', sEpisode)
+        oGui.addFolder(oGuiElement, oParams, False, True)
+
+    oGui.setEndOfDirectory()
+
+
+def loadInformation(mID = None):
+
+    if not mID:
+        return
+
+    oRequest = cRequestHandler(URL_REQUEST)
+
+    oRequest.addParameters('mID', mID)
+    oRequest.setRequestType(1)
+
+    sHtmlContent = oRequest.request()
+
+    if not sHtmlContent:
+        return
+
+    jContent = json.loads(sHtmlContent)
+
+    return [jContent[0]['plot'],
+            jContent[0]['cover'],
+            jContent[0]['year'],
+            jContent[0]['languages'][0]['symbol'],
+            jContent[0]['seasons'] if 'seasons' in jContent[0] else False]
 
 def showHosters():
     oParams = ParameterHandler()
@@ -68,20 +155,27 @@ def showHosters():
 
     oRequest.addParameters('mID', oParams.getValue('id'))
     oRequest.addParameters('raw', 'true')
-    oRequest.addParameters('language', 'en')
+    oRequest.addParameters('language', oParams.getValue('lang'))
+    if oParams.getValue('season'):
+        oRequest.addParameters('season', oParams.getValue('season'))
+        oRequest.addParameters('episode', oParams.getValue('episode'))
 
     sHtmlContent = oRequest.request()
-    isMatch, aResult = cParser().parse(sHtmlContent, 'name":"(.*?)".*?URL":"(.*?)"')
+
+    jContent = json.loads(sHtmlContent)
 
     hosters = []
-    if not isMatch:
+    if not jContent[1]:
         return hosters
 
-    for sName, sUrl in aResult:
-        hoster = dict()
-        hoster['link'] = sUrl.replace('\\', '')
-        hoster['name'] = sName
-        hosters.append(hoster)
+    for ohoster in jContent[1].items():
+        sName = ohoster[1]['name']
+
+        for links in ohoster[1]['links']:
+            hoster = dict()
+            hoster['link'] = links['URL']
+            hoster['name'] = sName
+            hosters.append(hoster)
 
     if hosters:
         hosters.append('getHosterUrl')
