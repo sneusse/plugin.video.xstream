5783a6859a451129f5d6fa0c71600774fe3eaf66
anis3 <anis3.n@web.de>
2016-09-29 17:53:49 +0200

    Add Hosters

--- sites/video2k_is.py
+++ sites/video2k_is.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-import json
-import threading
 from resources.lib.gui.gui import cGui
 from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.requestHandler import cRequestHandler
@@ -23,7 +20,6 @@ def load():
     logger.info("Load %s" % SITE_NAME)
     oGui = cGui()
     params = ParameterHandler()
-
     params.setParam('sUrl',URL_MOVIE % 'releases')
     oGui.addFolder(cGuiElement('Neu', SITE_IDENTIFIER, 'showEntries'), params)
     params.setParam('sUrl',URL_MOVIE % 'featured')
@@ -44,10 +40,7 @@ def showEntries(entryUrl = False, sGui = False):
     sHtmlContent = oRequestHandler.request()
 
     parser = cParser()
-    pattern = '<li[^>]*title="(.*?)\((:?\d+)?\).*?(:?\s+-\s+(:?[^"]+))?"[^>]*>.*?' # name / year / desc
-    pattern += "<a[^>]*href='([^']*)'[^>]*>.*?" # url
-    pattern += "<img[^>]*src='([^']*)'[^>]*>.*?" # img
-    pattern += "</li>" # title
+    pattern ='<li[^>]title="([^">]+).*?([^-/d]+)-film[^>]stream[^>]([^.]+).*?<img[^>]src=[^>]([^?<]+)[^>][^>]border.*?<a[^>]href=.*?>([^<]+)'
     isMatch, aResult = parser.parse(sHtmlContent, pattern)
 
     if not isMatch: 
@@ -55,23 +48,49 @@ def showEntries(entryUrl = False, sGui = False):
         return
 
     total = len (aResult)
-    for sName, sYear, sUglyDesc, sDesc, sUrl, sThumbnail in aResult:
-        oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showHosters')
-        oGuiElement.setThumbnail(sThumbnail)
-        oGuiElement.setYear(sYear)
-        oGuiElement.setDescription(sDesc)
-        oGui.addFolder(oGuiElement, params, False, total)
-
+    for sDescription, sYear, sUrl, sThumbnail, sName in aResult:
+            oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showHosters')
+            oGuiElement.setThumbnail(sThumbnail)
+            oGuiElement.setDescription(cUtil().unescape(sDescription.decode('utf-8')).encode('utf-8'))
+            oGuiElement.setYear(sYear)
+            params.setParam('entryUrl', 'http://video2k.is/?c=ajax&m=movieStreams2&id=' + sUrl)
+            params.setParam('sName', sName)
+            params.setParam('sThumbnail', sThumbnail)
+            oGui.addFolder(oGuiElement, params, bIsFolder = False,)
     isMatchNextPage, sNextUrl = parser.parseSingleResult(sHtmlContent, '</strong>.*?<a[^>]*href="([^"]+)"[^>]*>\d+')
     if isMatchNextPage:
         params.setParam('sUrl', cUtil().unescape(sNextUrl))
         oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
-
     if not sGui:
         oGui.setEndOfDirectory()
 
-def getHosters(sUrl = False):
-    return []
+def showHosters():
+    oParams = ParameterHandler()
+    sUrl = oParams.getValue('entryUrl')
+    sHtmlContent = cRequestHandler(sUrl).request()
+    sPattern = 'show[^>]player.*?, "([^"]+)".*?domain=([^">]+)[^>]'
+    aResult = cParser().parse(sHtmlContent, sPattern)
+    hosters = []
+    if aResult[1]:
+        for sUrl, sName in aResult[1]:
+            hoster = {}
+            hoster['link'] = sUrl
+            hoster['name'] = sName
+            hosters.append(hoster)         
+    if hosters:
+        hosters.append('getHosterUrl')
+    return hosters
+
+def getHosterUrl(sUrl = False):
+    oParams = ParameterHandler()
+    if not sUrl: sUrl = oParams.getValue('url')
+    logger.info("url %s" % sUrl)
+    results = []
+    result = {}
+    result['streamUrl'] = sUrl
+    result['resolved'] = False
+    results.append(result)
+    return results
 
 def showSearch():
     oGui = cGui()
