d43597303d3a84e91302eda99626ef93f0753de5
Viper2k4 <alexander.meinecke@googlemail.com>
2016-10-29 22:32:22 +0200

    [mykino.to] fix hosterlist

--- sites/mykino_to.py
+++ sites/mykino_to.py
@@ -33,81 +33,6 @@ def load():
     oGui.addFolder(cGuiElement('Suche', SITE_IDENTIFIER, 'showSearch'))
     oGui.setEndOfDirectory()
 
-def showEntries(entryUrl = False, sGui = False):
-    oGui = sGui if sGui else cGui()
-    params = ParameterHandler()
-    if not entryUrl: entryUrl = params.getValue('sUrl')
-    sHtmlContent = cRequestHandler(entryUrl, ignoreErrors = (sGui is not False)).request()
-    parser = cParser()
-    pattern = 'caption2"><a[^<]href="([^"]+).*?src="([^"]+).*?">([^<]+).*?<br>Jahr:[^<]([^<]+)'
-    
-    isMatch, aResult = parser.parse(sHtmlContent, pattern)
-
-    if not isMatch: 
-        if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
-        return
-
-    total = len (aResult)
-
-    for sUrl, sThumbnail,  sName, sYear in aResult:
-            oGuiElement = cGuiElement(cUtil().unescape(sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
-            oGuiElement.setThumbnail(sThumbnail)
-            oGuiElement.setYear(sYear)
-            params.setParam('entryUrl', sUrl)
-            oGui.addFolder(oGuiElement, params, False, total)
-
-    isMatchNextPage, sNextUrl = parser.parseSingleResult(sHtmlContent, '<a[^>]href="([^"]+)">Weiter</a>')
-    if isMatchNextPage:
-        params.setParam('sUrl', sNextUrl)
-        oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
-
-    if not sGui:
-        oGui.setEndOfDirectory()
-
-def showHosters():
-    oParams = ParameterHandler()
-    sUrl = oParams.getValue('entryUrl')
-    oRequestHandler = cRequestHandler(sUrl)
-    sHtmlContent = oRequestHandler.request()
-    hosters = []
-    parser = cParser()
-    isMatch, sContainer = parser.parseSingleResult(sHtmlContent, '<ul[^>]class="mirrors-selector".*?<ul class="mirrors-list[^>]loading">')
-    if not isMatch:
-        return hosters
-    isMatch, aResult = parser.parse(sContainer, '://([^/]+)([^#]+)')
-    if not isMatch:
-        return hosters
-    for sName, sUrl in aResult:
-            hoster = {}
-            hoster['name'] = sName.strip().replace('www.','')
-            hoster['link'] = 'http://' + sName + sUrl
-            hosters.append(hoster)
-
-    if hosters:
-        hosters.append('getHosterUrl')
-
-    return hosters
-
-def getHosterUrl(sUrl=False):
-    if not sUrl: sUrl = ParameterHandler().getValue('url')
-    results = []
-    result = {}
-    result['streamUrl'] = sUrl
-    result['resolved'] = False
-    results.append(result)
-    return results
-
-def showSearch():
-    oGui = cGui()
-    sSearchText = oGui.showKeyBoard()
-    if not sSearchText: return
-    _search(False, sSearchText)
-    oGui.setEndOfDirectory()
-
-def _search(oGui, sSearchText):
-    if not sSearchText: return
-    showEntries(URL_SEARCH % sSearchText.strip(), oGui)
-
 def showAlphaNumeric():
     sHtmlContent = cRequestHandler(URL_MAIN).request()
     pattern = '<div[^>]class="catalog-nav">.*?</div>'
@@ -150,3 +75,77 @@ def showGenre():
         oParams.setParam('sUrl', URL_MAIN + sUrl)
         oGui.addFolder(cGuiElement(sName , SITE_IDENTIFIER, 'showEntries'), oParams)
     oGui.setEndOfDirectory()
+
+def showEntries(entryUrl = False, sGui = False):
+    oGui = sGui if sGui else cGui()
+    params = ParameterHandler()
+    if not entryUrl: entryUrl = params.getValue('sUrl')
+    sHtmlContent = cRequestHandler(entryUrl, ignoreErrors = (sGui is not False)).request()
+    parser = cParser()
+    pattern = 'caption2"><a[^<]href="([^"]+).*?src="([^"]+).*?">([^<]+).*?<br>Jahr:[^<]([^<]+)'
+    
+    isMatch, aResult = parser.parse(sHtmlContent, pattern)
+
+    if not isMatch: 
+        if not sGui: oGui.showInfo('xStream','Es wurde kein Eintrag gefunden')
+        return
+
+    total = len (aResult)
+
+    for sUrl, sThumbnail,  sName, sYear in aResult:
+            oGuiElement = cGuiElement(cUtil().unescape(sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
+            oGuiElement.setThumbnail(sThumbnail)
+            oGuiElement.setYear(sYear)
+            params.setParam('entryUrl', sUrl)
+            oGui.addFolder(oGuiElement, params, False, total)
+
+    isMatchNextPage, sNextUrl = parser.parseSingleResult(sHtmlContent, '<a[^>]href="([^"]+)">Weiter</a>')
+    if isMatchNextPage:
+        params.setParam('sUrl', sNextUrl)
+        oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
+
+    if not sGui:
+        oGui.setEndOfDirectory()
+
+def showHosters():
+    sUrl = ParameterHandler().getValue('entryUrl')
+    sHtmlContent = cRequestHandler(sUrl).request()
+    
+    isMatch, aResult = cParser().parse(sHtmlContent, '<a[^>]*data-href="([^"]+)"[^>]*>.*?<span>([^<]*)<\/span>')
+    if not isMatch:
+        return []
+
+    hosters = []
+    for sUrls, sName in aResult:
+        urls = sUrls.split(',')
+        for idx, sLink in enumerate(sUrls.split(',')):
+            hoster = {}
+            hoster['name'] = sName
+            hoster['link'] = sLink
+            hoster['displayedName'] = '%s - Mirror %s' % (sName, idx+1)
+            hosters.append(hoster)
+
+    if hosters:
+        hosters.append('getHosterUrl')
+
+    return hosters
+
+def getHosterUrl(sUrl=False):
+    if not sUrl: sUrl = ParameterHandler().getValue('url')
+    results = []
+    result = {}
+    result['streamUrl'] = sUrl
+    result['resolved'] = False
+    results.append(result)
+    return results
+
+def showSearch():
+    oGui = cGui()
+    sSearchText = oGui.showKeyBoard()
+    if not sSearchText: return
+    _search(False, sSearchText)
+    oGui.setEndOfDirectory()
+
+def _search(oGui, sSearchText):
+    if not sSearchText: return
+    showEntries(URL_SEARCH % sSearchText.strip(), oGui)
