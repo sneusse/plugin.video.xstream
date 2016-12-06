ea0d9468f7fd9d61640393bc25230908924c07ae
StoneOffStones <irubin@me.com>
2016-12-06 20:46:03 +0100

    backup bad site-plugins

--- sites/cineplex/cineplex_tv.py
+++ sites/cineplex/cineplex_tv.py
@@ -5,7 +5,6 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib import logger
 from resources.lib.handler.ParameterHandler import ParameterHandler
-from resources.lib.handler.pluginHandler import cPluginHandler
 from resources.lib.util import cUtil
 import re
 
@@ -13,147 +12,168 @@ SITE_IDENTIFIER = 'cineplex_tv'
 SITE_NAME = 'Cineplex'
 SITE_ICON = 'cineplex.png'
 
-URL_MAIN = 'http://cineplex.tv/'
-URL_CINEMA = URL_MAIN + 'filme_2015/'
-URL_SERIE = URL_MAIN + 'serien/'
+URL_MAIN = 'http://cineplex.su/'
+URL_CINEMA2015 = URL_MAIN + 'filme_2015/'
+URL_CINEMA2014 = URL_MAIN + 'filme_2014/'
+URL_CINEMA2013 = URL_MAIN + 'filme_2013/'
 URL_SEARCH = URL_MAIN + 'index.php?story='
-
-#GENRES
-
-URL_GENRE_LIST = {'Abenteuer' : 'abenteuer/',
-                  'Action' : 'action/',
-                  'Animation' : 'animation/',
-                  'Drama' : 'drama/',
-                  'Fantasy' : 'fantasy/',
-                  'Horror' : 'horror/',
-                  'Krieg' : 'krieg/',
-                  'Kriminal' : 'kriminal/',
-                  'Komödie' : 'komoedie/',
-                  'Romanze' : 'romanze/',
-                  'Sci-Fi' : 'sci-fi/',
-                  'Sport' : 'sport/',
-                  'Thriller' : 'thriller/',
-                  'Western' : 'western/'}
+URL_GENRES_LIST = {'Abenteuer': 'abenteuer/', 'Action': 'action/', 'Animation': 'animation/', 'Drama': 'drama/',
+                   'Fantasy': 'fantasy/', 'Horror': 'horror/', 'Krieg': 'krieg/', 'Kriminal': 'kriminal/',
+                   'Komödie': 'komoedie/', 'Romanze': 'romanze/', 'Sci-Fi': 'sci-fi/', 'Sport': 'sport/',
+                   'Thriller': 'thriller/', 'Western': 'western/'}
+AZ_LIST = {'0-9': '/catalog/0-9/', 'A': '/catalog/A/', 'B': '/catalog/B/', 'C': '/catalog/C/', 'D': '/catalog/D/',
+           'E': '/catalog/E/', 'F': '/catalog/F/', 'G': '/catalog/G/', 'H': '/catalog/H/', 'I': '/catalog/I/',
+           'J': '/catalog/J/', 'K': '/catalog/K/', 'L': '/catalog/L/', 'M': '/catalog/M/', 'N': '/catalog/N/',
+           'O': '/catalog/O/', 'P': '/catalog/P/', 'Q': '/catalog/Q/', 'R': '/catalog/R/', 'S': '/catalog/S/',
+           'T': '/catalog/T/', 'U': '/catalog/U/', 'V': '/catalog/V/', 'W': '/catalog/W/', 'X': '/catalog/X/',
+           'Y': '/catalog/Y/', 'Z': '/catalog/Z/'}
 
 
 def load():
     logger.info("Load %s" % SITE_NAME)
     oGui = cGui()
-    oGui.addFolder(cGuiElement('Filme', SITE_IDENTIFIER, 'showMovieMenu'))
-    #oGui.addFolder(cGuiElement('Serien', SITE_IDENTIFIER, 'showTvShowMenu'))
-    #oGui.addFolder(cGuiElement('Schauspieler', SITE_IDENTIFIER, 'showActor'))
-    #oGui.addFolder(cGuiElement('Suche', SITE_IDENTIFIER, 'showSearch'))
-    oGui.setEndOfDirectory()
-
-def showSearch():
-    oGui = cGui()
-    sSearchText = oGui.showKeyBoard()
-
-    if not sSearchText: return
-    _search(oGui, sSearchText)
-
-def _search(oGui, sSearchText):
-    # Keine Eingabe? => raus hier
-    if not sSearchText: return
-
-    # URL-Übergeben und Ergebniss anzeigen
-    showEntries(URL_SEARCH + sSearchText.strip() + '&do=search&subaction=search')
-
-def showMovieMenu():
-    oGui = cGui()
     params = ParameterHandler()
-
-    params.setParam('sUrl', URL_CINEMA)
-    oGui.addFolder(cGuiElement('Aktuelle Kinofilme', SITE_IDENTIFIER, 'showEntries'), params)
     params.setParam('sUrl', URL_MAIN)
-    oGui.addFolder(cGuiElement('Genre', SITE_IDENTIFIER, 'showGenreList'), params)
-
+    oGui.addFolder(cGuiElement('Alle Filme', SITE_IDENTIFIER, 'showEntries'), params)
+    oGui.addFolder(cGuiElement('Genres', SITE_IDENTIFIER, 'showGenres'), params)
+    oGui.addFolder(cGuiElement('Erscheinungsjahr', SITE_IDENTIFIER, 'MoviesByYear'))
+    oGui.addFolder(cGuiElement('A-Z', SITE_IDENTIFIER, 'showAZ'))
+    oGui.addFolder(cGuiElement('Suche', SITE_IDENTIFIER, 'showSearch'))
     oGui.setEndOfDirectory()
 
-def showGenreList():
 
+def MoviesByYear():
     oGui = cGui()
-
-    for key in sorted(URL_GENRE_LIST):
-        params = ParameterHandler()
-        params.setParam('sUrl', (URL_MAIN + URL_GENRE_LIST[key]))
-        oGui.addFolder(cGuiElement(key, SITE_IDENTIFIER, 'showEntries'), params)
-
-
-    oGui.setEndOfDirectory()
-
-def showEntries(entryUrl=False):
-    oGui = cGui()
-
     params = ParameterHandler()
+    params.setParam('sUrl', URL_CINEMA2015)
+    oGui.addFolder(cGuiElement('Filme aus 2015', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_CINEMA2014)
+    oGui.addFolder(cGuiElement('Filme aus 2014', SITE_IDENTIFIER, 'showEntries'), params)
+    params.setParam('sUrl', URL_CINEMA2013)
+    oGui.addFolder(cGuiElement('Filme aus 2013', SITE_IDENTIFIER, 'showEntries'), params)
+    oGui.setEndOfDirectory()
 
+
+def showAZ():
+    oGui = cGui()
+    params = ParameterHandler()
+    for key in sorted(AZ_LIST):
+        params.setParam('sUrl', (URL_MAIN + AZ_LIST[key]))
+        oGui.addFolder(cGuiElement(key, SITE_IDENTIFIER, 'showEntries'), params)
+    oGui.setEndOfDirectory()
+
+
+def showGenres():
+    oGui = cGui()
+    params = ParameterHandler()
+    for key in sorted(URL_GENRES_LIST):
+        params.setParam('sUrl', (URL_MAIN + URL_GENRES_LIST[key]))
+        oGui.addFolder(cGuiElement(key, SITE_IDENTIFIER, 'showEntries'), params)
+    oGui.setEndOfDirectory()
+
+
+def showEntries(entryUrl=False, sGui=False):
+    oGui = sGui if sGui else cGui()
+    params = ParameterHandler()
     if not entryUrl: entryUrl = params.getValue('sUrl')
-
-    sHtmlContent = cRequestHandler(entryUrl).request()
-
-    pattern = 'li_block_title"><img\ssrc="(.+?(?=")).+?<a\shref="(.+?)">(.+?)\s\('
-
+    sHtmlContent = cRequestHandler(entryUrl, ignoreErrors=(sGui is not False)).request()
+    pattern = '<li>\s*<a[^>]*href="([^"]+)"[^>]*title="([^"]+)"[^>]*>\s*'  # url / title
+    pattern += '<img[^>]*src="([^"]*)"[^>]*>'  # thumbnail
     aResult = cParser().parse(sHtmlContent, pattern)
 
-    if len(aResult) > 0:
-
-        for result in aResult[1]:
-            oGuiElement = cGuiElement(result[2], SITE_IDENTIFIER, 'showHosters')
-            oGuiElement.setThumbnail(URL_MAIN + result[0])
-            params.setParam('entryUrl', result[1])
-            params.setParam('sName', result[2])
-            params.setParam('sThumbnail', result[0])
-            oGui.addFolder(oGuiElement, params, bIsFolder = False)
-
-        oGui.setEndOfDirectory()
-
+    if not aResult[0]:
+        if not sGui: oGui.showInfo('xStream', 'Es wurde kein Eintrag gefunden')
         return
+
+    total = len(aResult[1])
+    util = cUtil()
+    for sUrl, sName, sThumbnail in aResult[1]:
+        sName = util.unescape(sName.decode('utf-8')).encode('utf-8')
+        aYear = re.compile("(.*?)\((\d*)\)").findall(sName)
+        iYear = False
+        for name, year in aYear:
+            sName = name
+            iYear = year
+            break
+
+        oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showHosters')
+        oGuiElement.setThumbnail(sThumbnail)
+        oGuiElement.setMediaType('movie')
+        if iYear:
+            oGuiElement.setYear(iYear)
+        params.setParam('entryUrl', sUrl)
+        oGui.addFolder(oGuiElement, params, False, total)
+
+    if not sGui:
+        pattern = '<ul[^>]*class="pagination">.*?<li><a[^>]*href="([^"]*)">[^\d]+</a>\s*</li>\s*</ul>'
+        isMatch, sPageUrl = cParser.parseSingleResult(sHtmlContent, pattern)
+        if isMatch:
+            params.setParam('sUrl', sPageUrl)
+            oGui.addNextPage(SITE_IDENTIFIER, 'showEntries', params)
+
+        oGui.setView('movies')
+        oGui.setEndOfDirectory()
+        return
+
+
+def showSearchEntries(entryUrl=False, sGui=False):
+    oGui = sGui if sGui else cGui()
+    params = ParameterHandler()
+    if not entryUrl: entryUrl = params.getValue('sUrl')
+    sHtmlContent = cRequestHandler(entryUrl, ignoreErrors=(sGui is not False)).request()
+    pattern = 'class="short">.*?href="([^"]+)"[^>]class="title">([^<(]+)[^>]([^")]+).*?<img[^>]src="([^"]+)".*?>([^<]+)</p>'
+    aResult = cParser().parse(sHtmlContent, pattern)
+    if not aResult[0]:
+        if not sGui: oGui.showInfo('xStream', 'Es wurde kein Eintrag gefunden')
+        return
+    total = len(aResult[1])
+    util = cUtil()
+    for sEntryUrl, sName, sYear, sThumbnail, sDescription in aResult[1]:
+        oGuiElement = cGuiElement(util.unescape(sName.decode('utf-8')).encode('utf-8'), SITE_IDENTIFIER, 'showHosters')
+        oGuiElement.setThumbnail(sThumbnail)
+        oGuiElement.setDescription(util.unescape(sDescription.decode('utf-8')).encode('utf-8'))
+        oGuiElement.setYear(sYear)
+        oGuiElement.setMediaType('movie')
+        params.setParam('entryUrl', sEntryUrl)
+        oGui.addFolder(oGuiElement, params, False, total)
+    return
+
 
 def showHosters():
     params = ParameterHandler()
     sUrl = params.getValue('entryUrl')
-
     sHtmlContent = cRequestHandler(sUrl).request()
-    sHtmlContent = sHtmlContent.replace('\\', '')
-
-    sPattern = 'tab-pane\sfade(\sactive\sin)?"\sid="(.+?)"(.+?<\/div>)'
-    aResult = cParser().parse(sHtmlContent, sPattern, ignoreCase=True)
+    sPattern = '<div[^>]*role="tabpanel"[^>]*id="([^"]+)"[^>]*>\s*(?:<span|<a)(.*?)</div>'
+    aResult = cParser.parse(sHtmlContent, sPattern)
 
     hosters = []
+    if not aResult[1]:
+        return hosters
 
-    if len(aResult[1]) > 0:
-
-        for hoster in aResult[1]:
-            hPattern = '(http.*?)"'
-            hResult = cParser().parse(hoster[2], hPattern, ignoreCase=True)
-
-            if len(hResult[1]) > 0:
-                for link in hResult[1]:
-                    temphoster = {}
-                    temphoster['name'] = hoster[1]
-                    temphoster['link'] = link
-                    hosters.append(temphoster)
-
-    if len(hosters) > 0:
+    for entry in aResult[1]:
+        Pattern = 'href="([^"]+)"'
+        lResult = cParser.parse(entry[1], Pattern)
+        if not lResult[1]: continue
+        for i, link in enumerate(lResult[1], 1):
+            hosters.append({'name': entry[0], 'link': link, 'displayedName': '%s  Mirror %s' % (entry[0], i)})
+    if hosters:
         hosters.append('getHosterUrl')
-
     return hosters
 
-def getHosterUrl(sUrl = False):
-    #ParameterHandler erzeugen
-    oParams = ParameterHandler()
 
-    # URL ermitteln falls nicht übergeben
-    if not sUrl: sUrl = oParams.getValue('url')
+def getHosterUrl(sUrl=False):
+    if not sUrl: sUrl = ParameterHandler().getValue('url')
+    return {'streamUrl': sUrl, 'resolved': False}
 
-    logger.info("url %s" % sUrl)
 
-    # Array mit einem Eintrag für Hosterliste erzeugen (sprich direkt abspielen)
-    results = []
-    result = {}
-    result['streamUrl'] = sUrl
-    result['resolved'] = False
-    results.append(result)
+def showSearch():
+    oGui = cGui()
+    sSearchText = oGui.showKeyBoard()
+    if not sSearchText: return
+    _search(False, sSearchText)
+    oGui.setEndOfDirectory()
 
-    # Ergebniss zurückliefern
-    return results
+
+def _search(oGui, sSearchText):
+    if not sSearchText: return
+    showSearchEntries(URL_SEARCH + sSearchText.strip() + '&do=search&subaction=search', oGui)
